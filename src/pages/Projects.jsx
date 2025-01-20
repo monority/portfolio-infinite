fe5c110ee2f5e7b6a4d6ff469c6636dc39918c38
monority <chenu.pro@gmail.com>
2025-01-20 12:21:04 +0100

    NEW : FIx CSC PROJECT to todoapp

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -242,7 +242,7 @@ const Projects = () => {
 							</div>
 							<div className="project-wrapper">
 								<div className="element_title">
-									<h1>CSC</h1>
+									<h1>Todo App FulLStack</h1>
 								</div>
 								<div className="element_subtitle">
 									<svg stroke="currentColor" fill="none" strokeWidth="2" viewBox="0 0 24 24" strokeLinecap="round" strokeLinejoin="round" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><polyline points="16 18 22 12 16 6"></polyline><polyline points="8 6 2 12 8 18"></polyline></svg>
@@ -250,7 +250,7 @@ const Projects = () => {
 								</div>
 								<div className="element_content">
 									<p>
-										Website for a musician / artist showing off his differents creations and music.
+										Application to create todo list with user login and registration. Managing back-end with firebase.
 									</p>
 								</div>
 								<div className="element_row">
@@ -280,9 +280,9 @@ const Projects = () => {
 										</div>
 									</div>
 									<div className="element_link">
-										<div className="element_glob" onClick={() => window.open("https://github.com/monority/dev-baldursgate-game", "_blank")}>
+										<div className="element_glob" onClick={() => window.open("https://tdappnext.web.app/", "_blank")}>
 											<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" fill="currentColor" aria-hidden="true" width="2.5rem" height="2.5rem"><path fillRule="evenodd" d="M19.902 4.098a3.75 3.75 0 00-5.304 0l-4.5 4.5a3.75 3.75 0 001.035 6.037.75.75 0 01-.646 1.353 5.25 5.25 0 01-1.449-8.45l4.5-4.5a5.25 5.25 0 117.424 7.424l-1.757 1.757a.75.75 0 11-1.06-1.06l1.757-1.757a3.75 3.75 0 000-5.304zm-7.389 4.267a.75.75 0 011-.353 5.25 5.25 0 011.449 8.45l-4.5 4.5a5.25 5.25 0 11-7.424-7.424l1.757-1.757a.75.75 0 111.06 1.06l-1.757 1.757a3.75 3.75 0 105.304 5.304l4.5-4.5a3.75 3.75 0 00-1.035-6.037.75.75 0 01-.354-1z" clipRule="evenodd"></path></svg>
-											<p className="hover_text" >Details</p>
+											<p className="hover_text" >Incoming</p>
 										</div>
 
 									</div>
