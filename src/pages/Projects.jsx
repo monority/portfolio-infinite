b6b85327919cf77cb0f0157d1128c6abb33289ae
monority <chenu.pro@gmail.com>
2025-01-20 12:21:40 +0100

    FIX : Letter on word

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -242,7 +242,7 @@ const Projects = () => {
 							</div>
 							<div className="project-wrapper">
 								<div className="element_title">
-									<h1>Todo App FulLStack</h1>
+									<h1>Todo App FullStack</h1>
 								</div>
 								<div className="element_subtitle">
 									<svg stroke="currentColor" fill="none" strokeWidth="2" viewBox="0 0 24 24" strokeLinecap="round" strokeLinejoin="round" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><polyline points="16 18 22 12 16 6"></polyline><polyline points="8 6 2 12 8 18"></polyline></svg>
