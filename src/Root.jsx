e51efe1df2b12c09d475cf4c4c45ef45560120e7
monority <chenu.pro@gmail.com>
2025-01-22 21:00:37 +0100

    DELETE : Useless FIles

--- src/Root.jsx
+++ src/Root.jsx
@@ -2,21 +2,18 @@ import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 // Page
-import Home from './pages/Home'
 
 // Components
 
 import App from './App'
 import Footer from './components/Footer'
 import Header from './components/global/Header'
-import ProjectDetails from './pages/Project/ProjectDetails'
 const Root = () => {
 	return (
 		<BrowserRouter>
 			<Header></Header>
 			<Routes>
 				<Route exact path="/" element={<App />} />
-				<Route path="/project/:id" element={<ProjectDetails />} />
 			</Routes>
 			<Footer></Footer>
 		</BrowserRouter>
--- src/pages/Project/ProjectDetails.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const ProjectDetails = () => {
-  return (
-	<>ProjectDetails</>
-  )
-}
-
-export default ProjectDetails
